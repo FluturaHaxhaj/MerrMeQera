4f6f3302eea647caa7a0c581deb1b4daa659d5aa
endritbajrami <43909587+endritbajrami@users.noreply.github.com>
2020-06-18 23:46:42 +0200

    Dizajnimi i Menus

--- app/src/main/java/com/fiek/ppmapp/Dashboard.java
+++ app/src/main/java/com/fiek/ppmapp/Dashboard.java
@@ -1,18 +1,26 @@
 package com.fiek.ppmapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class Dashboard extends AppCompatActivity {
 
     String strExtras;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
 
         if (getIntent().getExtras() != null)
             strExtras = getIntent().getExtras().getString("emri");
