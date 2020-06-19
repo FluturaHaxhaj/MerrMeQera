40a9025d20114bd8ee16d57f3b9f155a85f62021
endritbajrami <43909587+endritbajrami@users.noreply.github.com>
2020-06-19 12:49:07 +0200

    Menu
    Shtimi i ikonave si dhe style-it te menu item-ave

--- app/src/main/java/com/fiek/ppmapp/Dashboard.java
+++ app/src/main/java/com/fiek/ppmapp/Dashboard.java
@@ -1,19 +1,25 @@
 package com.fiek.ppmapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class Dashboard extends AppCompatActivity {
+public class Dashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     String strExtras;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
+    ImageView menuIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +27,28 @@ public class Dashboard extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_dashboard);
 
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.navigation_view);
-
         if (getIntent().getExtras() != null)
             strExtras = getIntent().getExtras().getString("emri");
 
         Toast.makeText(Dashboard.this, strExtras + " jeni lloguar me sukses!", Toast.LENGTH_LONG).show();
 
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        menuIcon = findViewById(R.id.imageView2);
+
+
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+
+
+    }
+
+    public void openDrawer(){
+        drawerLayout.openDrawer(drawerLayout);
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return true;
     }
 }
